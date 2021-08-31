52a5fd6db552a689ce7a7db54f25b4bbff961105
DongHyunKIM-Hi <happybird00@naver.com>
2021-08-31 17:43:02 +0900

    feat: Stream 함수 탐구 시작

---
+++ src/test/java/com/example/pracjava/TestStream.java
@@ -0,0 +1,12 @@
+package com.example.pracjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestStream {
+    @Test
+    public void test1(){
+        System.out.println("tset");
+    }
+}
